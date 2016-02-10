2aa8285ca27abf7dbfeca090864065435dba4971
ylin91 <ylin91@10a5a6a3-9ba2-434f-bd16-b0f42afacb97>
2016-02-10 23:37:56 +0000

    add queue to database and add call to get from data base with callback to display them on software ylin91_mlin35

---
+++ _projects/QUA/trunk/quara/app/src/main/java/quara/test_login/GetQueueCallBack.java
@@ -0,0 +1,11 @@
+package quara.test_login;
+
+import java.util.Map;
+
+/**
+ * Created by ylin9 on 2/10/2016.
+ */
+interface GetQueueCallBack {
+    public abstract void done(Map returnQueue);
+}
+
