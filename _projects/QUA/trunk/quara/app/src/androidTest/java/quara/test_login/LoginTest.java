003c3920e8f3ebcdb8cae4a13df49c375b237523
pzhao12 <pzhao12@10a5a6a3-9ba2-434f-bd16-b0f42afacb97>
2016-02-22 06:13:02 +0000

    Instrumented test case for Login added ylin91_mlin35_pzhao12

---
+++ _projects/QUA/trunk/quara/app/src/androidTest/java/quara/test_login/LoginTest.java
@@ -0,0 +1,27 @@
+package quara.test_login;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+
+public class LoginTest extends ActivityInstrumentationTestCase2<Login>{
+    public LoginTest(){
+        super(Login.class);
+    }
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @SmallTest
+    public void testAuthenticateSuccess(){
+        User user = new User("testpzhao12","testpzhao12","testpzhao12");
+        this.getActivity().authenticate(user);
+        assertEquals(true, this.getActivity().userLocalStore.getUserLoggedIn());
+    }
+
+}
