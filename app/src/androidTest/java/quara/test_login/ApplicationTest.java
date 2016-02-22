bc73c54d376a1dfe0a7bb66297f48142d6327e77
pschu3 <pschu3@10a5a6a3-9ba2-434f-bd16-b0f42afacb97>
2016-02-22 00:10:48 +0000

    wortman2 and pschu3 adding story 2 tests

--- app/src/androidTest/java/quara/test_login/ApplicationTest.java
+++ app/src/androidTest/java/quara/test_login/ApplicationTest.java
@@ -2,6 +2,10 @@ package quara.test_login;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import org.junit.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -10,4 +14,22 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Test
+    public void testAddToQueue() {
+        Map<String, Boolean> names_on_queue = new HashMap<String, Boolean>();
+        names_on_queue.put("ALBERT", true);
+        boolean containsAlbert = names_on_queue.containsKey("ALBERT");
+        assertEquals(containsAlbert,true);
+    }
+
+    @Test
+    public void testDeleteFromQueue() {
+        Map<String, Boolean> names_on_queue = new HashMap<String, Boolean>();
+        names_on_queue.put("ALBERT",true);
+        names_on_queue.remove("ALBERT");
+        boolean containsAlbert = names_on_queue.containsKey("ALBERT");
+        assertEquals(containsAlbert,false);
+    }
 }
+
