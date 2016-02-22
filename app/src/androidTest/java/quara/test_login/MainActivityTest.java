b3116bc5cb08b9de543a1e0bb9b54bca004884ff
pzhao12 <pzhao12@10a5a6a3-9ba2-434f-bd16-b0f42afacb97>
2016-02-22 02:13:50 +0000

    Instrumented test case for UserLocalStore added ylin91_mlin35_pzhao12

---
+++ app/src/androidTest/java/quara/test_login/MainActivityTest.java
@@ -0,0 +1,37 @@
+package quara.test_login;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest(){
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @SmallTest
+    public void testUserLocalStoreStoreData(){
+        UserLocalStore usl = getActivity().userLocalStore;
+        User usr = new User("test name","test user","user pwd");
+        usl.storeUserData(usr);
+        assertNotNull(usl.getLoggedInUser());
+        User user = usl.getLoggedInUser();
+        assertEquals("test name", user.name);
+        usl.setUserLoggedIn(true);
+        assertTrue(usl.getUserLoggedIn());
+        usl.setUserLoggedIn(false);
+        assertFalse(usl.getUserLoggedIn());
+        usl.clearUserData();
+        assertEquals("",usl.getLoggedInUser().name);
+    }
+
+}
