0dce3f904a34972118a77f687d1f033027e708bc
ylin91 <ylin91@10a5a6a3-9ba2-434f-bd16-b0f42afacb97>
2016-03-03 02:40:52 +0000

    add gcm server code (not working version) ylin91_mlin35_pzhao12

--- _projects/QUA/trunk/quara/app/src/main/java/quara/test_login/Login.java
+++ _projects/QUA/trunk/quara/app/src/main/java/quara/test_login/Login.java
@@ -4,17 +4,26 @@ import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 import java.util.logging.Handler;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
@@ -58,11 +67,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         userLocalStore.storeUserData(returnUser);
         userLocalStore.setUserLoggedIn(true);
-
-        //Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTRATION");
-        //registrationIntent.putExtra("app", PendingIntent.getBroadcast(vt.getContext(), 0, new Intent(), 0));
-        //registrationIntent.putExtra("sender","470822730050");
-        //startService(registrationIntent);
 
         startActivity(new Intent(this, MainActivity.class));
     }
